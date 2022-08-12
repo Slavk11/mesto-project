93268292f8699f7055e49357fb71ccd9dcc68619
Slavk <slavk11@yandex.ru>
2022-08-12 22:59:23 +0300

    Реализовать редактирование имени и информации о себе

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const popupOpened = document.querySelector('.profile__edit-button');
 const closePopup = document.querySelector('.popup__close-button');
 const popup = document.querySelector('.popup')
-
+const page = document.querySelector('.page');
 popupOpened.addEventListener('click', function(){
   popup.classList.add('popup_opened');
 
@@ -10,4 +10,15 @@ popupOpened.addEventListener('click', function(){
 closePopup.addEventListener('click', function(){
   popup.classList.remove('popup_opened');
 
-})
+});
+const formElement = document.querySelector('.popup__user-form');
+const nameInput = document.querySelector('.popup__form_name');
+const jobInput = document.querySelector('.popup__form_job');
+const profileName = document.querySelector('.profile__info-title');
+const profileJob = document.querySelector('.profile__info-subtitle');
+function formSubmitHandler (evt) {
+    evt.preventDefault(); 
+    profileName.textContent = nameInput.value;
+    profileJob.textContent = jobInput.value;
+};
+formElement.addEventListener('submit', formSubmitHandler); 
